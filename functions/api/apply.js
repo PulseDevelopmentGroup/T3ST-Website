1c7bccb4bf2be1e6103a8e8d5ee6d9daf8ed980b
Carson Seese <me@carsonseese.com>
2022-01-06 17:22:27 +0000

    Integrate API into CF pages

---
+++ functions/api/apply.js
@@ -0,0 +1,33 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  switch (pathname) {
+    case "/api/apply":
+      const formData = await request.formData();
+
+      const email = formData.get("email");
+      const user = formData.get("ubisoftAccount");
+      const message = `A new nerd thinks he's good enough to join T3ST
+      
+Email: ${email}
+Username: ${user}`;
+
+      await fetch(WEBHOOK_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          content: message,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      return Response.redirect("https://t3st.games");
+    default:
+      return Response.redirect("https://t3st.games");
+  }
+}
